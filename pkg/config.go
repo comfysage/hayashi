d210db16e36bfb5396c9df5d0a2355db15cc298e
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2023-01-03 21:14:58 +0100

    add(pkg/config): added AddInstalled method to Config

--- pkg/config.go
+++ pkg/config.go
@@ -1,6 +1,7 @@
 package pkg
 
 import (
+	"fmt"
 	"io"
 
 	"gopkg.in/yaml.v3"
@@ -26,4 +27,17 @@ func (c Config) String() (string, error) {
 func (c *Config) FromString(r io.Reader) error {
 	d := yaml.NewDecoder(r)
 	return d.Decode(c)
+}
+
+func (c *Config) AddInstalled(pkg Pkg) error {
+	if pkg.Collection == "" {
+		return fmt.Errorf("no collection defined for pkg")
+	}
+	if pkg.Name == "" {
+		return fmt.Errorf("no name defined for pkg")
+	}
+	c.Installed = append(c.Installed, pkg.Collection + "/" + pkg.Name)
+	SaveConfig(*c)
+
+	return nil
 }
