b9cec38407ce1b503473d133cc0b1db9cfacfcbf
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2023-01-18 16:24:25 +0100

    fix(cli/exec): show stdout while running command

--- cli/exec.go
+++ cli/exec.go
@@ -2,10 +2,35 @@ package cli
 
 import (
 	"io"
+	"os"
 	"os/exec"
 	"path"
 	"strings"
+	"sync"
 )
+
+func copyAndCapture(w io.Writer, r io.Reader) ([]byte, error) {
+	var out []byte
+	buf := make([]byte, 1024, 1024)
+	for {
+		n, err := r.Read(buf[:])
+		if n > 0 {
+			d := buf[:n]
+			out = append(out, d...)
+			_, err := w.Write(d)
+			if err != nil {
+				return out, err
+			}
+		}
+		if err != nil {
+			// Read returns io.EOF at the end of file, which is not an error for us
+			if err == io.EOF {
+				err = nil
+			}
+			return out, err
+		}
+	}
+}
 
 func run(script []string, pwd string) error {
 	for _, c := range script {
@@ -15,9 +40,12 @@ func run(script []string, pwd string) error {
 			pwd = path.Join(el...)
 			continue
 		}
+
+		/* var stdout []byte
+		var errStdout error */
 		cmd := exec.Command(el[0], el[1:]...)
 		cmd.Dir = pwd
-		stdout, err := cmd.StdoutPipe()
+		stdoutpipe, err := cmd.StdoutPipe()
 		if err != nil {
 			return err
 		}
@@ -26,11 +54,18 @@ func run(script []string, pwd string) error {
 			return err
 		}
 
-		s, err := io.ReadAll(stdout)
+		var wg sync.WaitGroup
+		wg.Add(1)
+		go func() {
+			_, err = copyAndCapture(os.Stdout, stdoutpipe)
+			wg.Done()
+		}()
+
+		wg.Wait()
+
 		if err != nil {
 			return err
 		}
-		printf(string(s))
 
 		err = cmd.Wait()
 		if err != nil {
