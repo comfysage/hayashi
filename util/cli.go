79b65b86d71df4e96ebdea89419f3de8523be079
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2023-01-20 15:36:45 +0100

    add(util/cli): added HasFileExtension and HasCorrectFileExtension util functions

--- util/cli.go
+++ util/cli.go
@@ -42,3 +42,29 @@ func ExtendPath(path string) (string, error) {
 
 	return abspath, nil
 }
+
+func HasFileExtension(path string) (string, error) {
+	if len(path) < 1 {
+		return "", fmt.Errorf("path not long enough")
+	}
+
+	ext := filepath.Ext(path)
+	if len(ext) < 1 {
+		return "", fmt.Errorf("no extension specified")
+	}
+
+	return ext, nil
+}
+
+func HasCorrectFileExtension(path string, extension string) (bool, error) {
+	ext, err := HasFileExtension(path)
+	if err != nil {
+		return false, err
+	}
+
+	if ext != extension {
+		return false, fmt.Errorf("incorrect extension")
+	}
+
+	return true, nil
+}
