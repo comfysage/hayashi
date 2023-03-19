824a4ecff437497f856e539bced45a36f79c4df6
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2023-03-19 15:55:23 +0100

    add(pkg/store): added RemoveInstalled method for StoreFile struct

--- pkg/store.go
+++ pkg/store.go
@@ -46,3 +46,34 @@ func (c *StoreFile) AddInstalled(pkg Pkg) error {
 
 	return nil
 }
+
+func (c *StoreFile) RemoveInstalled(pkg Pkg) error {
+	if pkg.Collection == "" {
+		return fmt.Errorf("no collection defined for pkg")
+	}
+	if pkg.Name == "" {
+		return fmt.Errorf("no name defined for pkg")
+	}
+
+	pkgString := pkg.Collection + "/" + pkg.Name
+
+	// NOTE: If you do not care about ordering, you have the much faster
+	// possibility to replace the element to delete with the one at the end of
+	// the slice and then return the n-1 first elements.
+	//   ```go
+	//	 func remove(s []int, i int) []int {
+	//		 s[i] = s[len(s)-1]
+	//		 return s[:len(s)-1]
+	//	 }
+	//   ```
+	// If you want to keep your array ordered, you have to shift all of the
+	// elements at the right of the deleting index by one to the left.
+	for i, s := range c.Installed {
+		if s == pkgString {
+			c.Installed = append(c.Installed[:i], c.Installed[i+1:]...)
+		}
+	}
+	SaveStoreFile(*c)
+
+	return nil
+}
