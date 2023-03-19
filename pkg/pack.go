9aa6bdbf779cc62a34e6100da225f85d78d2fbe3
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2023-03-19 15:23:51 +0100

    add(pkg/pack): added CreatePack function

--- pkg/pack.go
+++ pkg/pack.go
@@ -1,6 +1,61 @@
 package pkg
 
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/crispybaccoon/hayashi/util"
+)
+
 type Pack struct {
 	Path string `yaml:"path"`
 	Type string `yaml:"type"`
+}
+
+func (pkg Pkg) CreatePack() error {
+	_, err := util.Mkdir(util.PACK_ROOT)
+	if err != nil {
+		return err
+	}
+
+	for _, p := range pkg.Pack {
+		if len(p.Path) == 0 || len(p.Type) == 0 {
+			continue
+		}
+
+		src := util.PathRepoFile(pkg.Name, p.Path)
+		dst := util.PathPackFile(p.Type, p.Path)
+
+		sourceFileStat, err := os.Stat(src)
+		if err != nil {
+			return err
+		}
+
+		if !sourceFileStat.Mode().IsRegular() {
+			return fmt.Errorf("%s is not a regular file", src)
+		}
+
+		source, err := os.Open(src)
+		if err != nil {
+			return err
+		}
+		defer source.Close()
+
+		_, err = util.Mkdir(util.PathPackDir(p.Type))
+		if err != nil {
+			return err
+		}
+
+		destination, err := os.Create(dst)
+		if err != nil {
+			return err
+		}
+		defer destination.Close()
+		_, err = io.Copy(destination, source)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
