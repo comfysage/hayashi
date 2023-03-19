e118806e4a8729f7ea8f2e5d1d813a6961e693e9
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2023-03-19 15:57:21 +0100

    add(pkg/pack): added RemovePack function; removes installed (packed) files

--- pkg/pack.go
+++ pkg/pack.go
@@ -59,3 +59,27 @@ func (pkg Pkg) CreatePack() error {
 	}
 	return nil
 }
+
+func (pkg Pkg) RemovePack() error {
+	for _, p := range pkg.Pack {
+		if len(p.Path) == 0 || len(p.Type) == 0 {
+			continue
+		}
+		dst := util.PathPackFile(p.Type, p.Path)
+
+		dstFileStat, err := os.Stat(dst)
+		if err != nil {
+			return err
+		}
+
+		if !dstFileStat.Mode().IsRegular() {
+			return fmt.Errorf("%s is not a regular file", dst)
+		}
+
+		err = os.Remove(dst)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
